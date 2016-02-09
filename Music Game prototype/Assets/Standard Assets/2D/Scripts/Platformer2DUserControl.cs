d038bb1e575360fd4d2df61a9174a021fa70cbf2
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-02-09 13:10:27 +0200

    keyboard controls

--- Music Game prototype/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Music Game prototype/Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -37,7 +37,15 @@ namespace UnityStandardAssets._2D
         {
             // Read the inputs.
             bool crouch = Input.GetKey(KeyCode.LeftControl);
-            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			float keyboard = CrossPlatformInputManager.GetAxis ("Horizontal");
+			float gamepad = CrossPlatformInputManager.GetAxis ("HorizontalGamepad");
+
+			float h;
+			if (Mathf.Abs (gamepad) > Mathf.Abs (keyboard)) {
+				h = gamepad;
+			} else {
+				h = keyboard;
+			}
             // Pass all parameters to the character control script.
             m_Character.Move(h, crouch, m_Jump);
 
