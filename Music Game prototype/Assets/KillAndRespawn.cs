280326bfc7c4f9c23a45a4e0666191f4b7772cbf
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-02-12 18:58:40 +0200

    respawn

---
+++ Music Game prototype/Assets/KillAndRespawn.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillAndRespawn : MonoBehaviour {
+
+	public Transform respawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		col.transform.position = respawn.position;
+	}
+}
