277ea29e5bfe69e53a4da31694c8cf0c87d7b9ef
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-02-17 14:05:22 +0200

    beat matching and flashing platforms on time

---
+++ Music Game prototype/Assets/BeatMatcher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeatMatcher : MonoBehaviour {
+
+	public ChangeColorOnBeat[] notifiables;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("ReportBeat", 0.46153846f, 0.46153846f);
+		notifiables = FindObjectsOfType<ChangeColorOnBeat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ReportBeat(){
+		foreach (ChangeColorOnBeat notify in notifiables) {
+			notify.Beat();
+		}
+
+	}
+}
---
+++ Music Game prototype/Assets/ChangeColorOnBeat.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorOnBeat : MonoBehaviour {
+
+	Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = gameObject.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Beat (){
+		if (rend.material.color == Color.green) {
+			rend.material.color = Color.black;
+		} else {
+			rend.material.color = Color.green;
+		}
+	}
+}
