66de99a1a573de761df1f24c787493d6cb6f34c2
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-03-21 16:02:33 +0200

    making the level better

--- Music Game prototype/Assets/Destructible.cs
+++ Music Game prototype/Assets/Destructible.cs
@@ -5,9 +5,13 @@ public class Destructible : MonoBehaviour, Quanter {
 
 	BeatMatcher beat;
 	public GameObject destructionEffect;
-
+	Renderer rend;
+	public Material cracked;
+	public Material destroyed;
+	
 	void Awake(){
 		beat = FindObjectOfType<BeatMatcher> ();
+		rend = gameObject.GetComponent<Renderer> ();
 	}
 
 	// Use this for initialization
@@ -22,10 +26,12 @@ public class Destructible : MonoBehaviour, Quanter {
 
 	void DestroyOnQuant(){
 		beat.registerQuantOneOff (this);
+		rend.material = cracked;
 	}
 
 	public void Act(){
 		Instantiate (destructionEffect);
-		gameObject.SetActive (false);
+		rend.material = destroyed;
+		gameObject.GetComponent<Collider2D> ().enabled = false;
 	}
 }
