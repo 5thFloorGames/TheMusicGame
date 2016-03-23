d0a9eb3d49875f4327a846c9a1085efdc7ca225d
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2016-03-23 18:23:11 +0200

    girl tests

---
+++ Music Game prototype/Assets/CreateBlock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateBlock : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (Resources.LoadAll<GameObject>("DashBlocks")[Random.Range(0,6)]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
